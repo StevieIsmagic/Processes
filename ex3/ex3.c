0b88ce09164c76c2ed70109d147c4823c7da77e9
Colombianito66 <colombianito66@gmail.com>
2017-10-10 02:42:03 -0400

    exercise 3 complete

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -2,6 +2,9 @@
 // the parent process should print "goodbye". You should ensure that the child
 // process always prints first.
 
+// TO COMPILE THIS PROGRAM >>> `gcc [NAME OF FILE].c -o [NAME OF EXECUTABLE]`
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,6 +12,19 @@
 int main(int argc, char* argv[])
 {
     // Your code here
+int status;
+int rc = fork();
 
+if (rc < 0) {
+    fprintf(stderr, "fork failed\n");
+    exit(1);
+} else if (rc == 0) {
+    printf("Hello!\n");
+} else {
+    while (waitpid(rc, &status, WNOHANG) == 0) {
+        sleep(1);
+    }
+    printf("Goodbye!\n");   
+}
     return 0;
 }
