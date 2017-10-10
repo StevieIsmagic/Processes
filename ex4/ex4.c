389038e11e40470b442746c19d86ec52d2ea8d2f
Colombianito66 <colombianito66@gmail.com>
2017-10-10 02:49:32 -0400

    exercise 4 complete

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -3,6 +3,8 @@
 // `execl()`, `execle()`, `execv()`, and others. Why do you think there 
 // are so many variants of the same basic call?
 
+// TO COMPILE THIS PROGRAM >>> `gcc [NAME OF FILE].c -o [NAME OF EXECUTABLE]`
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -10,6 +12,28 @@
 int main(int argc, char* argv[])
 {
     // Your code here    
+    printf("Parent process here\n");
+    int rc = fork();
 
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("Child process here\n");
+
+        // THESE ARE ALL DIFFERENT 'EXEC' METHODS AND DO THE SAME THING
+
+        // execl("/bin/ls", "ls", "-l" (char *) NULL);
+
+        // char *args[] = {"ls", "-l" (char *) NULL};
+        // execv("/bin/ls", args);
+
+        // execlp("ls", "ls", "-l", (char *) NULL);
+
+        char *args[] = {"ls", "-l", NULL};
+        execvp("ls", args);
+    } else {
+        wait(NULL);
+    }
     return 0;
 }
