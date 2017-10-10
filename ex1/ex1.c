e9e8b3bf6ca0cf2e37495039b6c51c27d7cf0ffc
Colombianito66 <colombianito66@gmail.com>
2017-10-10 02:28:48 -0400

    ex1 complete

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -2,6 +2,8 @@
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
 // What happens to the variable when both the child and parent change the value of x?
 
+// TO COMPILE THIS PROGRAM >>> `gcc [NAME OF FILE].c -o [NAME OF EXECUTABLE]`
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,6 +11,23 @@
 int main(int argc, char *argv[])
 {
     // Your code here
+    int x = 100;
+    printf("hello world (pid: %d)\n", (int) getpid());
+
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 1) {
+        printf("Hello, I am child (pid: %d) and x is: %d\n", (int) getpid(), x);
+        x++;
+        printf("Child again x is now: %d\n", x);
+    } else {
+        printf("Hello, I am parent of %d (pid: %d) and x is: %d\n", rc, (int) getpid(), x);
+        x--;
+        printf("Parent again, x is now: %d\n", x);
+    }
 
     return 0;
 }
